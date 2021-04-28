271d91b478b9e2e875f495f4febac56fdcfe9695
Svetlana0207 <buinova0207@gmail.com>
2021-04-28 15:21:23 +0800

    port

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "3000",
+  port: "443",
 });
 
  //let userName=document.getElementById('#name')
